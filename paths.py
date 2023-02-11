92dc272d91e4c494a22d22a9b68c18b904a70429
tedro <sktedro@gmail.com>
2023-02-11 21:19:06 +0100

    Migration to mmdet v3.0 looks successful
    TODO annotate.ipynb

--- paths.py
+++ paths.py
@@ -15,17 +15,17 @@ process_dataset_dirpath = os.path.join(proj_path, "process_dataset")
 mmdetection_config_path = os.path.join(mmdetection_path, "configs")
 mmdetection_checkpoint_path = os.path.join(mmdetection_path, "checkpoints")
 
-# model_config_filename = "yolox_x_8x8_300e_coco.py"
 # model_config_filename = "yolox_s_8x8_300e_coco.py"
-model_config_filename = "yolox_s_8x8_300e_coco_custom.py"
+model_config_filename = "yolox_s_8xb8-300e_coco_custom.py"
 # model_config_filename = "yolox_tiny_8x8_300e_coco.py"
 # model_config_filename = "yolox_nano_8x8_300e_coco.py"
 model_config_filepath = os.path.join(mmdetection_config_path, "yolox", model_config_filename)
+# model_config_filepath = os.path.join(mmdetection_config_path, "yolov8", "yolov8_m_syncbn_fast_8xb16-500e_coco_custom.py")
 
 # And YOLOX-x checkpoint
-# model_checkpoint_filename = "yolox_x_8x8_300e_coco_20211126_140254-1ef88d67.pth"
 model_checkpoint_filename = "yolox_s_8x8_300e_coco_20211121_095711-4592a793.pth"
 # model_checkpoint_filename = "yolox_tiny_8x8_300e_coco_20211124_171234-b4047906.pth"
+# model_checkpoint_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_20230115_192200-c22e560a.pth"
 # model_checkpoint_filename = "yolox_nano.pth"
 model_checkpoint_filepath = os.path.join(mmdetection_path, "checkpoints", model_checkpoint_filename)
 if not os.path.exists(mmdetection_checkpoint_path):
