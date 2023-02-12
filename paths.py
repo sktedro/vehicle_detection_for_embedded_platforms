516226c0adb898b82bdf8129a29bdc1a6bc42aa9
tedro <sktedro@gmail.com>
2023-02-13 00:29:34 +0100

    Training YOLOX with MMYOLO. Only tested on CPU

--- paths.py
+++ paths.py
@@ -5,13 +5,12 @@ proj_path = os.getcwd().replace("\\", "/") # Convert backslashes to forward slas
 # Parent folder of mmlabs repositories
 mm_parent_dirpath = os.path.join(proj_path, "..")
 
-working_dirpath = os.path.join(proj_path, "working_dir")
+working_dirpath = os.path.join(proj_path, "working_dir_yolox_mmyolo")
 
-# model_config_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_custom.py"
-model_config_filename = "yolov8_s_syncbn_fast_8xb16-500e_coco_custom.py"
-model_config_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "configs", "yolov8", model_config_filename)
+model_config_filename = "yolox_s_8xb8-300e_coco_custom.py"
+model_config_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "configs", "yolox", model_config_filename)
 
-model_checkpoint_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_20230115_192200-c22e560a.pth"
+model_checkpoint_filename = "yolox_s_8xb8-300e_coco_20220917_030738-d7e60cb2.pth"
 model_checkpoint_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "checkpoints", model_checkpoint_filename)
 
 last_checkpoint_link = os.path.join(working_dirpath, "last_checkpoint")
