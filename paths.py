e2c279df1572959f27fff1a468159bc2f333c759
sktedro <sktedro@gmail.com>
2023-02-14 23:45:27 +0100

    Training works but detector can't seem to detect
    ... anything

--- paths.py
+++ paths.py
@@ -5,13 +5,13 @@ proj_path = os.getcwd().replace("\\", "/") # Convert backslashes to forward slas
 # Parent folder of mmlabs repositories
 mm_parent_dirpath = os.path.join(proj_path, "..")
 
-working_dirpath = os.path.join(proj_path, "working_dir_yolov8")
+working_dirpath = os.path.join(proj_path, "working_dir_yolov5")
 
 # model_config_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_custom.py"
-model_config_filename = "yolov8_s_syncbn_fast_8xb16-500e_coco_custom.py"
-model_config_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "configs", "yolov8", model_config_filename)
+model_config_filename = "yolov5_m-v61_syncbn_fast_8xb16-300e_coco_custom.py"
+model_config_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "configs", "yolov5", model_config_filename)
 
-model_checkpoint_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_20230115_192200-c22e560a.pth"
+model_checkpoint_filename = "yolov5_m-v61_syncbn_fast_8xb16-300e_coco_20220917_204944-516a710f.pth"
 model_checkpoint_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "checkpoints", model_checkpoint_filename)
 
 last_checkpoint_link = os.path.join(working_dirpath, "last_checkpoint")
