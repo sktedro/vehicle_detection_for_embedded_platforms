dfa145e1a8991b500447b219464ee2f018be618b
sktedro <sktedro@gmail.com>
2023-02-19 15:01:39 +0100

    YOLOv8 training working, both medium and nano

--- paths.py
+++ paths.py
@@ -6,12 +6,14 @@ proj_path = os.getcwd().replace("\\", "/") # Convert backslashes to forward slas
 mm_parent_dirpath = os.path.join(proj_path, "..")
 
 working_dirpath = os.path.join(proj_path, "working_dir_yolov8")
+# working_dirpath = os.path.join(proj_path, "working_dir_yolov8_n")
 
-# model_config_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_custom.py"
-model_config_filename = "yolov8_s_syncbn_fast_8xb16-500e_coco_custom.py"
+model_config_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_custom.py"
+# model_config_filename = "yolov8_n_syncbn_fast_8xb16-500e_coco_custom.py"
 model_config_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "configs", "yolov8", model_config_filename)
 
 model_checkpoint_filename = "yolov8_m_syncbn_fast_8xb16-500e_coco_20230115_192200-c22e560a.pth"
+# model_checkpoint_filename = "yolov8_n_syncbn_fast_8xb16-500e_coco_20230114_131804-88c11cdb.pth"
 model_checkpoint_filepath = os.path.join(mm_parent_dirpath, "mmyolo", "checkpoints", model_checkpoint_filename)
 
 last_checkpoint_link = os.path.join(working_dirpath, "last_checkpoint")
