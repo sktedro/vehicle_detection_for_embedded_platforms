b2c2c6508f47045492bd99742f9918d584db1422
sktedro <sktedro@gmail.com>
2023-04-24 17:58:24 +0000

    Inference ONNXRuntime now supports batch size

--- inference_onnxruntime.py
+++ inference_onnxruntime.py
@@ -29,6 +29,17 @@ DEFAULT_DEVICE = "cpu"
 # TODO use MM visualizer?
 
 def main(args):
+    # Basic assertions
+    assert os.path.exists(args.work_dir), "Working dir does not exist: " + args.work_dir
+    assert args.device in ["cpu", "cuda"], "Incorrect device name in --device"
+    assert os.path.exists(args.input), "Input video not found: " + args.input
+    assert args.step >= 1
+    assert args.batch_size >= 1
+    assert args.number >= -1
+    assert 0 <= args.threshold <= 1
+    if args.multi_thread and args.device == "cuda":
+        print("Multi-thread mode selected but running on CUDA. Ignoring")
+
     video = mmcv.VideoReader(args.input)
 
     # Update args
@@ -63,62 +74,82 @@ def main(args):
     session = onnxruntime.InferenceSession(model_filepath, session_options, providers=[provider])
     session.get_modelmeta()
     session_input_name = session.get_inputs()[0].name
-    detector_input_shape = session.get_inputs()[0].shape[::-1][:2]
+    detector_input_shape = session.get_inputs()[0].shape[::-1][:2] # w*h
+    dynamic_batch_size = session.get_inputs()[0].shape[0] != 1
+    assert dynamic_batch_size or args.batch_size == 1, "Batch size needs to be 1 for static models"
     print("Detector input shape:", detector_input_shape)
+    print("Allow dynamic batch size:", dynamic_batch_size)
 
     print("Reading and annotating images")
     inference_durations = []
     try:
 
         pbar = tqdm(range(args.number))
-        for i in pbar:
+        for i in range(0, args.number, args.batch_size): # Progress bar will be updated manually based on the batch size
+
+            frames_orig = []
+            frames_resized = []
+            frames_preprocessed = []
 
             # Read a frame
-            # frame = video[i * args.step] # This doesn't work well :/
-            frame_orig = video.read()
-            for _ in range(args.step - 1): # This fixes it
-                video.read()
+            for _ in range(args.batch_size):
 
-            if frame_orig is None:
+                # This is necessary - indexing in the video doesn't work well
+                frame = video.read()
+                for _ in range(args.step - 1): # Skip frames according to step
+                    video.read()
+
+                if frame is None:
+                    break
+
+                frames_orig.append(frame)
+
+                # Resize to detector input shape
+                frame = mmcv.imresize(frame, detector_input_shape)
+                frames_resized.append(frame)
+
+                # Pre-process
+                frame = frame.transpose(2, 0, 1).astype(np.float32) / 255.
+                # frame = np.expand_dims(frame, axis=0)
+                frames_preprocessed.append(frame)
+
+            if len(frames_orig) == 0:
                 break
 
-            # Resize to detector input shape
-            frame_resized = mmcv.imresize(frame_orig, detector_input_shape)
-
-            # Pre-process
-            frame = frame_resized.transpose(2, 0, 1)
-            frame = frame.astype(np.float32)
-            frame /= 255.
-            frame = np.expand_dims(frame, axis=0)
+            detector_input = np.array(frames_preprocessed)
 
             # Run the inference and measure the duration
             if args.device == "cpu":
                 start = time.process_time()
-                bboxes, labels = session.run(None, {session_input_name: frame})
+                bboxes, labels = session.run(None, {session_input_name: detector_input})
                 inference_durations.append(time.process_time() - start)
             else:
                 start = time.time()
-                bboxes, labels = session.run(None, {session_input_name: frame})
+                bboxes, labels = session.run(None, {session_input_name: detector_input})
                 inference_durations.append(time.time() - start)
 
-            bboxes = np.array(bboxes[0])
-            labels = np.array(labels[0])
-            for j in range(len(bboxes)):
-                if bboxes[j][4] < args.threshold:
-                    continue
+            for img_in_batch_i in range(len(frames_resized)):
+                frame = frames_resized[img_in_batch_i]
+                for detection_i in range(len(bboxes[img_in_batch_i])):
+                    if bboxes[img_in_batch_i][detection_i][4] < args.threshold:
+                        continue
 
-                x1, y1, x2, y2 = bboxes[j][:4].astype(int)
-                confidence = round(bboxes[j][4] * 100)
-                frame_resized = cv2.rectangle(frame_resized, (x1, y1), (x2, y2), (0, 255, 0), 1)
-                frame_resized = cv2.putText(frame_resized, str(confidence) + ": cls=" + str(labels[j]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+                    x1, y1, x2, y2 = bboxes[img_in_batch_i][detection_i][:4].astype(int)
+                    confidence = round(bboxes[img_in_batch_i][detection_i][4] * 100)
+                    frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                    frame = cv2.putText(frame, str(confidence) + ": cls=" + str(labels[img_in_batch_i][detection_i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
 
-            # Save the image
-            out_img_filename = str(i).zfill(6) + ".jpg"
-            out_img_filepath = os.path.join(out_img_dirpath, out_img_filename)
-            cv2.imwrite(out_img_filepath, frame_resized)
+                # Save the image
+                img_number = i + img_in_batch_i
+                out_img_filename = str(img_number).zfill(6) + ".jpg"
+                out_img_filepath = os.path.join(out_img_dirpath, out_img_filename)
+                cv2.imwrite(out_img_filepath, frames_resized[img_in_batch_i])
+
+            # Update pbar
+            pbar.update(args.batch_size)
 
             # Update pbar description - average inference duration
-            avg_duration = sum(inference_durations) / len(inference_durations)
+            avg_duration = sum(inference_durations) / len(inference_durations) / args.batch_size
             if args.device == "cpu":
                 pbar.set_description(f"Avg inference CPU duration: {'%.3f' % avg_duration}s")
             else:
@@ -129,11 +160,14 @@ def main(args):
     except KeyboardInterrupt:
         print("KeyboardInterrupt: Stopped annotating images")
 
+    del pbar
+
     if len(inference_durations):
+        avg_duration = sum(inference_durations) / len(inference_durations) / args.batch_size
         if args.device == "cpu":
-            print("Average inference CPU duration:", sum(inference_durations) / len(inference_durations))
+            print("Average inference CPU duration (per sample):", avg_duration)
         else:
-            print("Average inference real duration:", sum(inference_durations) / len(inference_durations))
+            print("Average inference real duration (per sample):", avg_duration)
 
     print("Converting to video")
     try:
@@ -162,6 +196,8 @@ if __name__ == "__main__":
                         help=f"input video file. Default {DEFAULT_INPUT}")
     parser.add_argument("-s", "--step",         type=int,   default=1,
                         help="image step size (every step'th image will be taken). Default 1")
+    parser.add_argument("-b", "--batch-size",   type=int,   default=1,
+                        help="Inference batch size. Model needs to be dynamic to allow for it! Default 1")
     parser.add_argument("-n", "--number",       type=int,   default=-1,
                         help="number of frames to annotate. Default -1 to annotate all")
     parser.add_argument("-t", "--threshold",    type=float, default=DEFAULT_THRESHOLD,
@@ -171,15 +207,5 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--multi-thread", action="store_true",
                         help="multi-threaded inference (when on cpu)")
     args = parser.parse_args()
-
-    # Basic assertions
-    assert os.path.exists(args.work_dir), "Working dir does not exist: " + args.work_dir
-    assert args.device in ["cpu", "cuda"], "Incorrect device name in --device"
-    assert os.path.exists(args.input), "Input video not found: " + args.input
-    assert args.step > 0
-    assert args.number >= -1
-    assert 0 <= args.threshold <= 1
-    if args.multi_thread and args.device == "cuda":
-        print("Multi-thread mode selected but running on CUDA. Ignoring")
 
     main(args)
