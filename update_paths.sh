790662f9b99dde77a42ee9fa79470536aba2481b
user <sktedro@gmail.com>
2023-04-26 01:04:24 +0200

    update_paths to use sed to update absolute paths
    ... in all python files in the project directory or one step under. Eg.
    to replace `/home/user/dataset/` to `/home/user/Downloads/dataset/` or
    something

---
+++ update_paths.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+in_dir(){
+    for f in "$1"/*; do
+        if [ -f "$f" ] && [ ${f##*.} = "py" ]; then
+            if [ -z "$2" ] || [ -z "$3" ]; then
+                echo "$f"
+            else
+                echo "Processing $f"
+                CMD="s~$2~$3~g"
+                # echo $CMD
+                sed -i "$CMD" "$f"
+            fi
+        fi
+    done
+}
+
+echo "Changing all occurances of $1 to $2 in these files:"
+
+in_dir "."
+in_dir "configs"
+in_dir "deploy"
+for d in ./work*; do
+    if [ -d "$d" ]; then
+        in_dir "$d"
+    fi
+done
+
+read -p "Are you sure? (y/n)" choice
+case "$choice" in
+  y)
+    in_dir "." "$1" "$2"
+    in_dir "configs" "$1" "$2"
+    in_dir "deploy" "$1" "$2"
+    for d in ./work*; do
+        if [ -d "$d" ]; then
+            in_dir "$d" "$1" "$2"
+        fi
+    done
+    ;;
+  *)
+    echo "cancelling"
+    ;;
+esac
