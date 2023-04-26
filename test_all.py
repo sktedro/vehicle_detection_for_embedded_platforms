b97a879b00d3a82e811f5cfcb5a1113ce3046e57
tedro <sktedro@gmail.com>
2023-04-26 21:14:53 +0200

    Improved test scripts

--- test_all.py
+++ test_all.py
@@ -33,6 +33,76 @@ def run_task(args, logger):
     logger.info("Task done: " + args["filepath"])
 
 
+def get_all_tasks(args, logger):
+    # We can use get_all_tasks() from deploy_all.py
+    deploy_all_args = dotdict({
+        "deploy_jobs": 1,
+        "backend": args.backend,
+        "replace_existing": True
+        })
+    deploy_tasks = deploy_all.get_all_tasks(deploy_all_args, logger)
+
+    # TODO use batch size in log file
+    tasks = [ # Examples:
+        {
+            "deploy_cfg": ".../deploy/config_onnxruntime_static.py",
+            "work_dir": ".../<working_dirname>",
+            "filepath": "onnxruntime_static.onnx",
+            "device": "cuda",
+            "batch_size": 4,
+            "interval": 0,
+            "warmup": 10,
+            "speed_test": True,
+        }
+    ]
+    tasks = []
+    for working_dirname in deploy_tasks:
+        for config in deploy_tasks[working_dirname]["configs"]:
+            for batch_size in args.batch_sizes:
+                tasks.append({
+                    "deploy_cfg": config["config_filepath"],
+                    "work_dir": deploy_tasks[working_dirname]["working_dirpath"],
+                    "filepath": config["output_filename"],
+                    "device": args.device,
+                    "batch_size": batch_size,
+                    "visualize": args.visualize,
+                    "warmup": args.warmup,
+                    "speed_test": True,
+                    })
+
+    return tasks
+
+def filter_tasks(args, tasks, logger):
+    # Filter to existing tasks
+    for task in tasks.copy():
+        model_filepath = os.path.join(task["work_dir"], task["filepath"])
+        if not os.path.exists(model_filepath):
+            logger.warning(f"Model at {model_filepath} does not exist! Skipping")
+            tasks.remove(task)
+
+    # Filter out already done tasks
+    if not args.replace_existing:
+        for task in tasks.copy():
+            log_filepath = get_logfile(task)
+            if os.path.exists(log_filepath):
+                logger.warning(f"Test log at {log_filepath} found! Skipping")
+                tasks.remove(task)
+
+    return tasks
+
+
+def get_tasks(args, logger):
+    all_tasks = get_all_tasks(args, logger)
+    return filter_tasks(args, all_tasks, logger)
+
+
+def get_logfile(task):
+    return test_deployed.get_log_file(
+        task["work_dir"],
+        os.path.basename(task["filepath"]).replace(".", "_"),
+        task["batch_size"])
+
+
 def main(args):
     print("If you also want to log everything the subprocesses output, please use bash redirection to your own file. Logger here only logs output from this file")
     time.sleep(1)
@@ -67,61 +137,7 @@ def main(args):
         logger.warning("Test on CPU requested with TensorRT backend. Ignoring and using CUDA")
         args.device = "cuda"
 
-    # We can use get_all_tasks() from deploy_all.py
-    deploy_all_args = dotdict({
-        "deploy_jobs": 1,
-        "backend": args.backend,
-        "replace_existing": True
-        })
-    deploy_tasks = deploy_all.get_all_tasks(deploy_all_args, logger)
-
-    tasks = [ # Examples:
-        {
-            "deploy_cfg": ".../deploy/config_onnxruntime_static.py",
-            "work_dir": ".../<working_dirname>",
-            "filepath": "onnxruntime_static.onnx",
-            "device": "cuda",
-            "batch_size": 4,
-            "interval": 0,
-            "warmup": 10,
-            "speed_test": True,
-            "log2file": ".../<working_dirname>/onnxruntime_static_log"
-        }
-    ]
-    tasks = []
-    for working_dirname in deploy_tasks:
-        for config in deploy_tasks[working_dirname]["configs"]:
-            for batch_size in args.batch_sizes:
-                tasks.append({
-                    "deploy_cfg": config["config_filepath"],
-                    "work_dir": deploy_tasks[working_dirname]["working_dirpath"],
-                    "filepath": config["output_filename"],
-                    "device": args.device,
-                    "batch_size": batch_size,
-                    "visualize": args.visualize,
-                    "warmup": args.warmup,
-                    "speed_test": True,
-                    })
-
-    def get_logfile(task):
-        model_filename = os.path.basename(task["filepath"])
-        log_filename = f"test_{model_filename.replace('.', '_')}_log"
-        return os.path.join(task["work_dir"], log_filename)
-
-    # Filter to existing tasks
-    for task in tasks.copy():
-        model_filepath = os.path.join(task["work_dir"], task["filepath"])
-        if not os.path.exists(model_filepath):
-            logger.warning(f"Model at {model_filepath} does not exist! Skipping")
-            tasks.remove(task)
-
-    # Filter out already done tasks
-    if not args.replace_existing:
-        for task in tasks.copy():
-            log_filepath = get_logfile(task)
-            if os.path.exists(log_filepath):
-                logger.warning(f"Test log at {log_filepath} found! Skipping")
-                tasks.remove(task)
+    tasks = get_tasks(args, logger)
 
     logger.info("Tasks:")
     logger.info(pformat(tasks))
--- test_deployed.py
+++ test_deployed.py
@@ -45,8 +45,12 @@ def parse_args():
     #                     help=f"COCO dataset filepath to test with. Default {DEFAULT_TEST_DATASET}")
     parser.add_argument("-b", "--batch-size",   type=int, default=1,
                         help="test batch size. Default 1")
-    parser.add_argument("-v", "--visualize",        action="store_true",
+    parser.add_argument("-v", "--visualize",    action="store_true",
                         help="visualize the test ouput to a directory (in the work dir)")
+    parser.add_argument("-o", "--out-dir",      type=str,
+                        help="dir to output visualized images to. Relative to work dir. If not provided, will be generated")
+    parser.add_argument("-l", "--log-file", type=str,
+                        help="file to write test logs into. Relative to work dir. If not provided, will be generated")
     parser.add_argument("-w", "--warmup",       type=int, default=10,
                         help="warmup during speed test. Speed is not calculated for the first w samples. Default 10")
     parser.add_argument("-s", "--speed-test",   action="store_true",
@@ -54,6 +58,14 @@ def parse_args():
     args = parser.parse_args()
 
     return args
+
+
+def get_out_dir(work_dir, model_name, batch_size):
+    return os.path.join(work_dir, f"test_{model_name}_batch{batch_size}")
+
+
+def get_log_file(work_dir, model_name, batch_size):
+    return get_out_dir(work_dir, model_name, batch_size) + ".log"
 
 
 def main(args):
@@ -71,14 +83,16 @@ def main(args):
 
     model_name = os.path.basename(model_filepath).replace(".", "_")
 
-    out_dirpath = os.path.join(args.work_dir, "test_" + model_name)
-    log_dirpath = os.path.join(args.work_dir, "test_" + model_name + "_log")
-
     assert os.path.exists(args.deploy_cfg), f"Deploy config not found at {args.deploy_cfg}"
     assert os.path.exists(args.work_dir), f"Working dir not found at {args.work_dir}"
     assert args.device == "cpu" or args.device.startswith("cuda")
     assert args.warmup >= 0
     assert args.batch_size > 0
+
+    if args.out_dir is None:
+        args.out_dir = get_out_dir(args.work_dir, model_name, args.batch_size)
+    if args.log_file is None:
+        args.log_file = get_log_file(args.work_dir, model_name, args.batch_size)
 
     # Build a dotdict for the test.py script to act as args
     # All args need to be here - argparse returns default if argument is not
@@ -88,13 +102,13 @@ def main(args):
             "work_dir": args.work_dir,
             "model_cfg": model_cfg,
             "model": [model_filepath],
-            "show_dir": out_dirpath if args.visualize else None,
-            "log2file": log_dirpath,
             "device": args.device,
             "speed_test": args.speed_test,
             "cfg_options": {},
-            "show": False,
             "wait_time": 2, # Default 2
+            "show_dir": args.out_dir if args.visualize else None,
+            "log2file": args.log_file,
+            "show": False,
             "interval": 0 if args.visualize != 0 else 1,
             "warmup": args.warmup,
             "log_interval": 100, # Default
