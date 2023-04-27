c849d2eab3231d3c6ef6c81fcbabbbd6444a7723
Patrik Skaloš <xskalo01@gmail.com>
2023-04-27 19:07:41 +0200

    Improved collect_test_results to run even if tests are running

--- collect_test_results.py
+++ collect_test_results.py
@@ -25,6 +25,58 @@ import sys
 from pprint import pprint
 
 
+model_name_regex = r"^working_dir_(.*)_(\d+)x(\d+)"
+fps_regex = r".*times per count: [\d.]* ms, ([\d.]*) FPS"
+
+
+def parse_file(f, results):
+    work_dirname = os.path.basename(os.path.dirname(f))
+    model_name, w, h = re.match(model_name_regex, work_dirname).groups()
+    input_size = f"{w}x{h}"
+
+    # details example: [test onnxruntime dynamic batch onnx batch2]
+    details = os.path.basename(f).split(".")[0].split("_")
+    backend = details[1]
+    shape = details[2]
+    batch = details[-1].replace("batch", "")
+
+    with open(f) as fd:
+        content = fd.readlines()
+        if content == []:
+            raise Exception(f"File {f} is empty")
+
+        test_results = {}
+
+        # Get mAP values from the last line in the file
+        last_line = content[-1].replace("  ", " ").replace("  ", " ").split(" ")
+        for i in range(len(last_line)):
+            if "coco/" in last_line[i]:
+                mAP_name = last_line[i].split("/")[1].split(":")[0]
+                mAP_val = last_line[i+1]
+                test_results[mAP_name] = mAP_val
+        for s in ["bbox_mAP", "bbox_mAP_50", "bbox_mAP_75", "bbox_mAP_s", "bbox_mAP_m", "bbox_mAP_l"]:
+            assert s in list(test_results.keys()), f"{s} missing in the last line of the file {f}"
+
+        # Get average FPS from the last line containing "FPS" in the file
+        for line in reversed(content):
+            if line.endswith("FPS\n"):
+                test_results["fps"] = re.match(fps_regex, line).groups()[0]
+                break
+        else:
+            raise Exception(f"FPS not found in file {f}")
+
+    # Save the data
+    if model_name not in results:
+        results[model_name] = {}
+    if input_size not in results[model_name]:
+        results[model_name][input_size] = {}
+    if backend not in results[model_name][input_size]:
+        results[model_name][input_size][backend] = {}
+    if shape not in results[model_name][input_size][backend]:
+        results[model_name][input_size][backend][shape] = {}
+    results[model_name][input_size][backend][shape][batch] = test_results
+
+
 def main():
     assert len(sys.argv) > 1, "Please provide the test device as an argument to this script"
     device = sys.argv[1]
@@ -53,54 +105,14 @@ def main():
     print("Test files:")
     pprint(test_files)
 
-    model_name_regex = r"^working_dir_(.*)_(\d+)x(\d+)"
-    fps_regex = r".*times per count: [\d.]* ms, ([\d.]*) FPS"
-
+    exceptions = []
     results = {}
     for f in test_files:
-        work_dirname = os.path.basename(os.path.dirname(f))
-        model_name, w, h = re.match(model_name_regex, work_dirname).groups()
-        input_size = f"{w}x{h}"
-
-        # details example: [test onnxruntime dynamic batch onnx batch2]
-        details = os.path.basename(f).split(".")[0].split("_")
-        backend = details[1]
-        shape = details[2]
-        batch = details[-1].replace("batch", "")
-
-        with open(f) as fd:
-            content = fd.readlines()
-
-            test_results = {}
-
-            # Get mAP values from the last line in the file
-            last_line = content[-1].replace("  ", " ").replace("  ", " ").split(" ")
-            for i in range(len(last_line)):
-                if "coco/" in last_line[i]:
-                    mAP_name = last_line[i].split("/")[1].split(":")[0]
-                    mAP_val = last_line[i+1]
-                    test_results[mAP_name] = mAP_val
-            for s in ["bbox_mAP", "bbox_mAP_50", "bbox_mAP_75", "bbox_mAP_s", "bbox_mAP_m", "bbox_mAP_l"]:
-                assert s in list(test_results.keys()), f"{s} missing in the last line of the file {f}"
-
-            # Get average FPS from the last line containing "FPS" in the file
-            for line in reversed(content):
-                if line.endswith("FPS\n"):
-                    test_results["fps"] = re.match(fps_regex, line).groups()[0]
-                    break
-            else:
-                raise Exception(f"FPS not found in file {f}")
-
-        # Save the data
-        if model_name not in results:
-            results[model_name] = {}
-        if input_size not in results[model_name]:
-            results[model_name][input_size] = {}
-        if backend not in results[model_name][input_size]:
-            results[model_name][input_size][backend] = {}
-        if shape not in results[model_name][input_size][backend]:
-            results[model_name][input_size][backend][shape] = {}
-        results[model_name][input_size][backend][shape][batch] = test_results
+        try:
+            parse_file(f, results)
+        except Exception as e:
+            print(f"Exception occurred when parsing file {f}: {str(e)}. Ignoring")
+            exceptions.append(str(e))
 
     # Put the device name at the top
     results = {
@@ -114,6 +126,13 @@ def main():
 
     with open("test_all_results.json", "w") as f:
         f.write(results_json)
+
+    if exceptions == []:
+        print(f"All done, no exceptions")
+    else:
+        print(f"All done, {len(exceptions)} exceptions:")
+        for e in exceptions:
+            print(e)
 
 
 if __name__ == "__main__":
